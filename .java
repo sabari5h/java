6b3a6141510f19c0b9f43f1cb3632ed1682db0a3
sabari5h <144336501+sabari5h@users.noreply.github.com>
2023-11-06 08:56:50 -0800

    Add files via upload

--- .java
+++ .java
@@ -1,48 +1,74 @@
-import java.util.Scanner;
-
-class ScoreException extends Exception
+import java.io.*;
+class filedemo
 {
-    public ScoreException(String message)
-    {
-        super(message);
-    }
+public static void p(String str)
+{
+    System.out.println(str);
+ }
+ public static void analyze(String s)
+{
+File f=new File(s);
+ if(f.exists())
+ {
+ p(f.getName()+" is a file");
+ p(f.canRead()?" is readable":" is not readable");
+ p(f.canWrite()?" is writable":" is not writable");
+ p("Filesize:"+f.length()+" bytes");
+ p("File last mdified:"+f.lastModified());
+ }
+ if(f.isDirectory())
+ {
+ p(f.getName()+" is directory");
+ p("List of files");
+ String dir[]=f.list();
+ for(int i=0;i<dir.length;i++)
+ p(dir[i]);
+ }
+ }
+
+}
+public class FileDetails
+{
+ public static void main(String rr[])throws IOException
+ {
+ filedemo fd=new filedemo();
+ BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+ System.out.println("Enter the file name:");
+ String s=br.readLine();
+ fd.analyze(s);
+ }
 }
 
-public class TestScore
-{
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        String[] studid = {"A1", "B2", "C3", "D4", "E5"};
-        int[] scores = new int[studid.length];
-        for (int i = 0; i < studid.length; i++) {
-            System.out.print("Enter the test score for student " + studid[i] + ": ");
-            try
-            {
-                int score = Integer.parseInt(sc.nextLine());
-                if (score < 0 || score > 100)
-                {
-                    throw new ScoreException("Invalid score! Score must be between 0 and 100.");
-                }
-                scores[i] = score;
-            }
-            catch (NumberFormatException e)
-            {
-                System.err.println("Invalid input! Please enter a numeric score.");
-                scores[i] = 0;
-            }
-            catch (ScoreException e)
-            {
-                System.err.println(e.getMessage());
-                scores[i] = 0;
-            }
+import java.io.*;
+
+public class BinaryFileDemo {
+
+    public static void main(String[] args) {
+        try (FileOutputStream fos = new FileOutputStream("data.bin");
+             DataOutputStream dos = new DataOutputStream(fos)) {
+            dos.writeInt(100);
+            dos.writeDouble(3.14);
+            dos.writeBoolean(true);
+            dos.writeUTF("Hello");
+            System.out.println("Data written to the file.");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        System.out.println("\nStudent ID\tTest Score");
-        for (int i = 0; i < studid.length; i++) {
-            System.out.println(studid[i] + "\t\t" + scores[i]);
+        // Read the data from the file
+        try (FileInputStream fis = new FileInputStream("data.bin");
+             DataInputStream dis = new DataInputStream(fis)) {
+            int i = dis.readInt();
+            double d = dis.readDouble();
+            boolean b = dis.readBoolean();
+            String s = dis.readUTF();
+            System.out.println("Data read from the file:");
+            System.out.println("i = " + i);
+            System.out.println("d = " + d);
+            System.out.println("b = " + b);
+            System.out.println("s = " + s);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        sc.close();
     }
 }
